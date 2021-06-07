c184964c0408b1888538ad00552274dadd3e886b
weiliang1234 <2663107564@qq.com>
2021-06-07 20:03:13 +0800

    commit study04

---
+++ cloud-gateway-gateway9527/src/main/java/com/pdsu/springcloud/GateWayMain9527.java
@@ -0,0 +1,17 @@
+package com.pdsu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wl
+ * @Date 2021/6/7 17:50
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
